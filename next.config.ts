a16302b76cc7ce87cb297685a9f56fcc634d8e7c
brenokf <breno-kf@hotmail.com>
2026-05-16 01:19:02 -0400

    chore: disable caching in webpack configuration for improved build performance

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   webpack: (config, { isServer }) => {
+    config.cache = false;
+
     if (isServer && config.output) {
       config.output.chunkFilename = "[name].js";
     }
